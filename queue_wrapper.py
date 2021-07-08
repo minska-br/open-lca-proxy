0d0ef1a874644dbc250be302095fc11182d1a90e
Thales Pereira <thalespereiradejesus@gmail.com>
2021-07-07 22:02:41 -0300

    Create wrapper to communicate with AWS SQS queues
    * Initially we are only doing the local tests, so we keep the fixed endpoint URL to the local environment

---
+++ queue_wrapper.py
@@ -0,0 +1,27 @@
+import logging
+
+import boto3
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
+
+sqs = boto3.resource(
+    'sqs',
+    endpoint_url= "http://localhost" + ":4566"
+)
+
+def get_queue(name):
+    """
+    Gets an SQS queue by name.
+
+    :param name: The name that was used to create the queue.
+    :return: A Queue object.
+    """
+    try:
+        queue = sqs.get_queue_by_name(QueueName=name)
+        logger.info("Got queue '%s' with URL=%s", name, queue.url)
+    except ClientError as error:
+        logger.exception("Couldn't get queue named %s.", name)
+        raise error
+    else:
+        return queue
