fce1efb400435f2916d0c3bf1cb12de1af2ad966
Thales Pereira <thalespereiradejesus@gmail.com>
2021-07-07 22:05:42 -0300

    Create method to perform the calculation completed events
    * We created the "calculation-completed" queue to send messages containing all calculations performed for the food products that will be sent in our API

---
+++ producer.py
@@ -0,0 +1,37 @@
+import json
+import boto3
+
+import logging
+import queue_wrapper
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+logger = logging.getLogger(__name__)
+sqs = boto3.resource('sqs')
+
+def send_message(message_body, message_attributes=None):
+    """
+    Send a message to an Amazon SQS queue.
+
+    :param message_body: The body of the message.
+    :param message_attributes: Custom attributes of the message. These are key-value
+                               pairs that can be whatever you want.
+    :return: The response from SQS that contains the assigned message ID.
+    """
+    queue = queue_wrapper.get_queue('calculation-completed')
+    
+    if not message_attributes:
+        message_attributes = {}
+
+    try:
+        response = queue.send_message(
+            MessageBody= message_body,
+            MessageAttributes=message_attributes
+        )
+    except ClientError as error:
+        logger.exception("Send message failed: %s", message_body)
+        raise error
+    else:
+        return response
