8c133f4336d7330ccaf1e035170d252e5c84c3a9
Thales Pereira <thalespereiradejesus@gmail.com>
2021-10-12 19:13:31 -0300

    Add new conversions and change calculation methodology

--- main.py
+++ main.py
@@ -139,16 +139,19 @@ def _calculate_for_product(process_name: str, product: Product):
     logger.info('Perform calculation setup')
     setup.amount = amount_kg
     setup.calculation_type = olca.CalculationType.UPSTREAM_ANALYSIS
-    setup.impact_method = open_lca_client.find(olca.ImpactMethod, 'IPCC 2013 GWP 100a')
+    setup.impact_method = open_lca_client.find(olca.ImpactMethod, 'Greenhouse Gas Protocol')
     setup.product_system = open_lca_client.find(olca.ProductSystem, process_name)
 
     logger.info(f'Starts the calculation process for the product: {process_name}')
     
     calc_result = None
+    total_calculation = 0
     
     try:
         calc_result = open_lca_client.calculate(setup)
-        logger.info(f'Calculation completed for the product: {process_name} with the result: {calc_result.impact_results[0].value}')
+                
+        total_calculation = calc_result.impact_results[0].value + calc_result.impact_results[1].value + calc_result.impact_results[3].value
+        logger.info(f'Calculation completed for the product: {process_name} with the result: {total_calculation}')
     except Exception as e:
         logger.error(e)
         raise NoProcessesFound(f"Error calculating product: {process_name}")
@@ -159,9 +162,9 @@ def _calculate_for_product(process_name: str, product: Product):
     return ProcessCalculation(
         name=product.name,
         process_name_found=process_name,
-        value=calc_result.impact_results[0].value,
+        value=total_calculation,
         amount=amount_kg,
-        unit=calc_result.impact_results[0].impact_category.ref_unit,
+        unit="kg CO2 eq",
         calculated=True
     )
 
